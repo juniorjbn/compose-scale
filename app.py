e92c8cf95c769ad7551fa8f251ffb558bdd0108d
joao brito junior <juniorjbn@gmail.com>
2016-09-23 12:45:32 -0300

    Update app.py
    port changed

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello():
     return '\nHello World!\nI have been seen %s times.\n' % (redis.get('hits'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("80"), debug=True)
+    app.run(host="0.0.0.0", port=int("8080"), debug=True)
