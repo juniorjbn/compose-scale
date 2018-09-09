11257a77c3a1f23436722aac89d4cf6203025fc7
Joao <juniorjbn@gmail.com>
2018-09-09 20:27:39 -0300

    update msg

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ host = socket.gethostname()
 
 @app.route('/')
 def hello():
-    return '\nHello YOU! Im one of skynet running Container - name %s\n\n' % (host)
+    return '\nHello #V3! Im one of skynet running Container - name %s\n\n' % (host)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("8080"), debug=True)
